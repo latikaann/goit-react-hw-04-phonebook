c59387ed8977f64f7d7c3827dbf871866bf4942e
Anna Shalar <113612033+latikaann@users.noreply.github.com>
2023-05-11 21:06:58 +0300

    upd json, add files

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import css from '../ContactForm/ContactForm.module.css';
+import { nanoid } from 'nanoid';
+
+class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.addContact({ id: nanoid(6), ...this.state });
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({
+      name: '',
+      number: '',
+    });
+  };
+
+  render() {
+    return (
+      <div className={css.formBox}>
+        <form className={css.form} onSubmit={this.handleSubmit}>
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+              onChange={this.handleChange}
+              value={this.state.name}
+            />
+          </label>
+          <label>
+            Mobile
+            <input
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+              onChange={this.handleChange}
+              value={this.state.number}
+            />
+          </label>
+          <button className={css.formBtn} type="submit">
+            Add contact
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default ContactForm;
